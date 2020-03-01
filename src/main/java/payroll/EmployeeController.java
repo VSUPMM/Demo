2eb5b1ab06a65b4bbfa921512e85b5f38ac4f83d
VSUPMM <valyafenris@gmail.com>
2020-03-02 00:15:07 +0300

    fdb
    dsrv

---
+++ src/main/java/payroll/EmployeeController.java
@@ -0,0 +1,60 @@
+package payroll;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+
+@RestController
+class EmployeeController {
+
+    private final EmployeeRepository repository;
+
+    private final EmployeeModelAssembler assembler;
+
+    EmployeeController(EmployeeRepository repository,
+                       EmployeeModelAssembler assembler) {
+
+        this.repository = repository;
+        this.assembler = assembler;
+    }
+
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
+    }
+
+    @GetMapping("/employees/{id}")
+    EntityModel<Employee> one(@PathVariable Long id) {
+
+        Employee employee = repository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return assembler.toModel(employee);
+    }
+
+    @GetMapping("/employees")
+    CollectionModel<EntityModel<Employee>> all() {
+
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+
+        return new CollectionModel<>(employees,
+                linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+
+}
---
+++ src/main/java/payroll/LoadDatabase.java
@@ -0,0 +1,22 @@
+package payroll;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class LoadDatabase {
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository, OrderRepository orderRepo) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+            log.info("Preloading " + orderRepo.save(new Order("Task1", Status.COMPLETED)));
+            log.info("Preloading " + orderRepo.save(new Order("Task2", Status.IN_PROGRESS)));
+        };
+    }
+}
